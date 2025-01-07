7416b79ce9ea9a9df15f8035371ab05526b9992b
Avay12 <avaygc@gmail.com>
2025-01-07 10:47:19 +0545

    Implemente Messages Guard

--- DatingApp.Client/src/app/app.routes.ts
+++ DatingApp.Client/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { HomeComponent } from './features/home/home.component';
 import { MemberListsComponent } from './features/members/member-lists/member-lists.component';
 import { ListsComponent } from './features/lists/lists.component';
 import { MessagesComponent } from './features/messages/messages.component';
+import { authGuard } from './core/guard/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'members', component: MemberListsComponent },
+  {
+    path: 'members',
+    component: MemberListsComponent,
+    canActivate: [authGuard],
+  },
   { path: 'members/:id', component: MemberListsComponent },
   { path: 'lists', component: ListsComponent },
   { path: 'messages', component: MessagesComponent },
